cd651893be23c9cb5bf7ac84dd5932083b9460c5
David Levy <davidlevy@Davids-MacBook-Air.local>
2024-12-15 18:14:33 -0500

    final

--- ingestion.py
+++ ingestion.py
@@ -13,3 +13,23 @@ urls = [
 ]
 
 docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=250, chunk_overlap=0
+)
+
+docs_splits = text_splitter.split_documents(docs_list)
+
+# vectorstore = Chroma.from_documents(
+#     documents=docs_splits,
+#     collection_name="rag-chroma",
+#     embedding=OpenAIEmbeddings(),
+#     persist_directory="./.chroma",
+# )
+
+retriever = Chroma(
+    collection_name="rag-chroma",
+    persist_directory="./.chroma",
+    embedding_function=OpenAIEmbeddings(),
+).as_retriever()
