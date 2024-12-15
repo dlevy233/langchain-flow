1fbf4b600cbaf113057eb0d5c6682fe17c60eb19
David Levy <davidlevy@Davids-MacBook-Air.local>
2024-12-15 16:21:07 -0500

    initial

---
+++ ingestion.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
